28377c014b75e1253f09d3078f0aff0221e003ac
Keshav Sundar <keshavsundar4@gmail.com>
2026-03-07 15:27:02 -0500

    Update launch.py

--- UI/launch.py
+++ UI/launch.py
@@ -1,5 +1,13 @@
 import napari
 from pathlib import Path
+import sys
+
+ROOT = Path(__file__).resolve().parent
+SRC = ROOT / "src"
+if str(SRC) not in sys.path:
+    sys.path.insert(0, str(SRC))
+    
+from pathlib import Path
 from qtpy.QtCore import QTimer
 from biovision_napari.ui.main_widget import BioVisionWidget
 from biovision_napari.ui.comparison_panel import ComparisonPanel
